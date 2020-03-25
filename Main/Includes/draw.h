668e343fdd1c8697b06e6c2ff0e65fe12ad1f3b5
Martin <fejtimartin98@gmail.com>
2020-03-25 17:17:43 +0100

    - Add draw.h header file

---
+++ Main/Includes/draw.h
@@ -0,0 +1,16 @@
+#include "model.h"
+
+/**
+ * Draws the given model (1.param)
+ */
+void draw_model(const struct Model* model);
+
+/**
+ * Draws the triangles of the model.
+ */
+void draw_triangles(const struct Model* model);
+
+/**
+ * Draws the quads of the model.
+ */
+void draw_quads(const struct Model* model);
